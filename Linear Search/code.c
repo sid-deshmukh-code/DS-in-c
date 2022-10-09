c7ada91bbeab2187fec07c40c0062679bde2bd0a
siddhant <deshmukhsiddhant420@gmail.com>
2022-10-09 11:46:25 -0500

    some change in linear search

--- Linear Search/code.c
+++ Linear Search/code.c
@@ -4,20 +4,20 @@ void main()
 {
     
     int a[4], item, i;
-    printf("Enter list of numbers ");
+    printf("Enter list of numbers\n");
     for (i = 0; i < 4; i++)
     {
         scanf("%d",& a[i]);
     }
 
 
-    printf("Enter item to find ");
+    printf("Enter item to find\n");
     scanf("%d",& item);
 
     for (i = 0; i < 4; i++)
     {
         if (a[i]==item)
-            printf("Item found at %d \n ",i);
+            printf("Item found at index %d \n",i);
         
     }
     
